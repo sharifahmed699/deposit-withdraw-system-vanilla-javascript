01d2ea4570702f8d0937370eb47ba724950c14d2
sharifahmed699 <sharifahmedewu15@gmail.com>
2021-08-20 03:34:06 +0600

    valided input field

--- js/bank.js
+++ js/bank.js
@@ -13,29 +13,36 @@ function updateAmount(fieldId, amount){
     return total
 
 }
+function getCurrentBalance(){
+    const balanceTotal = document.getElementById('balance-total')
+    balanceText = balanceTotal.innerText
+    previousbalance = parseFloat(balanceText)
+    return previousbalance
+}
 function updateBalance(amount,isAdd){
     const balanceTotal = document.getElementById('balance-total')
+    const  previousbalance = getCurrentBalance()
     if(isAdd == true){
-        balanceTotal.innerText=parseFloat(balanceTotal.innerText) + parseFloat(amount)
+        balanceTotal.innerText=previousbalance +  parseFloat(amount)
     }
     else{
-        balanceTotal.innerText=parseFloat(balanceTotal.innerText) - parseFloat(amount)
+        balanceTotal.innerText=previousbalance-  parseFloat(amount)
     }
     
 }
 // handel deposit button event
 document.getElementById('deposite-button').addEventListener('click', function(){
-    depositAmount=getInputValue('deposit-input')
-
-    depositTotal = updateAmount('deposit-total', depositAmount)
-
+   const depositAmount=getInputValue('deposit-input')
+   if(depositAmount > 0){
+    updateAmount('deposit-total', depositAmount)
     updateBalance(depositAmount, true)
-   
+   }
 })
 document.getElementById('withdraw-button').addEventListener('click',function(){
-    withdrawAmount=getInputValue('withdraw-input')
-    
+   const withdrawAmount=getInputValue('withdraw-input')
+   const currentbalance = getCurrentBalance()
+   if(withdrawAmount > 0  && withdrawAmount < currentbalance){
     withdrawTotal  = updateAmount('withdraw-total', withdrawAmount)
-
     updateBalance(withdrawAmount, false)
+   }
 })
